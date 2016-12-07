87b300ed736ea5541e2b0c91f526af5f04aa6ddd
Axel Thor <axelthorstein@gmail.com>
2016-12-07 09:26:56 -0500

    added nickname phrases, imporoved phrases, keywords, and non repeating sentences

--- CREATE.sql
+++ CREATE.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Names (
 	ID SERIAL,
 	name VARCHAR(50),
+	nick_name VARCHAR(50),
 	gender VARCHAR(1),
 	country VARCHAR(2),
 	attributes VARCHAR(3),
--- generate_insert.py
+++ generate_insert.py
@@ -3,7 +3,7 @@
 import itertools
 import random
 
-def generate_SQL_insert(file_name, gender, country_code, attributes):
+def generate_SQL_insert_random(file_name, gender, country_code, attributes):
 	""" (str, str, str, list of str) -> NoneType
 
 	Create a file of SQL Insert statements for each name in the given list,
@@ -29,37 +29,102 @@ def generate_SQL_insert(file_name, gender, country_code, attributes):
 	input_file.close()
 	output_file.close()
 
+def generate_SQL_insert_chosen(file_name, gender, country_code):
+	""" (str, str, str, list of str) -> NoneType
 
-def create_bio(name, attributes):
+	Create a file of SQL Insert statements for each name in the given list,
+	for a specific gender and country. For each name assign 3 attributes
+	so that they are evenly distributed among names. 
+	"""
+
+	input_file = open(file_name, 'r')
+	output_file = open('FILL.sql', 'w')
+
+	names = input_file.readlines()
+
+	for name in names:
+		name_attribute = name.split()
+		name = name_attribute[0].strip()
+		nick_name = name_attribute[1].strip()
+		attributes = name_attribute[2].strip()
+
+		output_file.write("INSERT INTO Names (name, nick_name, gender, country, attributes) VALUES (\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\');\n".format(name, nick_name, gender, country_code, attributes))
+ 
+	input_file.close()
+	output_file.close()
+
+
+def create_bio(names, attributes):
 	""" (str, str) -> str
 
 	Create a bio using the chosen name and attributes.
 	"""
-	
-	attribute_names = {'O': 'outgoing', 'A': 'adventerous', 'B': 'bold', 'S': 'shy', 'T': 'tough', 'L': 'likeable', 'C': 'creative', 'F': 'funny'}
+	new_names = []
+	for i in names:
+		new_names.append(i.split(' '))
 
-	attribute_sentences = {'O': ['likes to go out', 'likes having fun', 'likes being spontaneous'], 
-	'A': ['isn\'t afraid to get their hands dirty', 'is always interested in a little adventure', 'can build a fire just with the friction of their bare hands'], 
-	'B': ['are not afraid to speak their mind', 'always tell people what they really think', 'takes chances'], 
-	'S': ['are more introverted', 'like to do things on their own', 'are a lone wolf'], 
-	'T': ['don\'t get scared easily', 'are muscular', 'look intimidating'], 
-	'L': ['are well liked', 'have a lot of friends', 'are very socialble'], 
-	'C': ['likes building', 'likes making things', 'likes being artistic'], 
-	'F': ['is always joking around', 'is the class clown', 'likes telling jokes']}
+	attribute_names = {'A': 'rebellious', 'B': 'low-key', 'C': 'simple', 'D': 'clever', 'E': 'introverted', 'F': 'extroverted', 'G': 'bold', 'H': 'funny'}
 
-	attributes_list = list(attributes)
+	nickname_sentence = '{0} is usually shorted to {1} by friends and family.\n'
 
-	bio = '{0} is a name usually associated with people who {1} and {2}. {0}\'s usually {3}.\n'.format(
-		name, 
-		random.choice(attribute_sentences[attributes_list[0]]), 
-		random.choice(attribute_sentences[attributes_list[1]]),  
-		random.choice(attribute_sentences[attributes_list[2]]))
+	attribute_sentences = {
+	'A': ['are very good at expressing themselves',
+	 'are typically not interested in being normal',
+	 'have very determined personalities',
+	 'are more willing to do something different'], 
+	'B': ['do not actively seek attention from others',
+	 'are capable of leaving things unsaid', 
+	 'tend to enjoy quality time with few people rather than with a large groups'], 
+	'C': ['are extremely easy to get along with',
+	 'find great joy in small things',
+	 'are able to bring peace into other peoples lives'], 
+	'D': ['are quick thinkers',
+	 'have a very logical response to the world around them',
+	 'are able to communicate very well with the people around them, which usually brings them success in their career'], 
+	'E': ['are very busy thinkers',
+	 'do not reveal their best qualities when they first meet new people',
+	 'make great partners and long lasting friends'], 
+	'F': ['gain energy when they are around other people',
+	 'are a very social person',
+	 'socialize very well with other people which makes them enjoyable people to be around'], 
+	'G': ['are the first person to try something others are scared to try',
+	 'love to try new things',
+	 'are great entrepreneuers which bring great change to the people around them'], 
+	'H': ['enjoys to make people laugh',
+	 'are always in a good mood',
+	 'make friends very easily thanks to their happy personality']}
 
-	return bio
+	bio_format = '{0} is a name usually associated with people who {1} and {2}. {0}\'s usually {3}. '
+
+	bios = []
+
+	for i in range(3):
+		
+		attributes_list = list(attributes)
+		first_pick = random.choice(attribute_sentences[attributes_list[0]])
+		second_pick = random.choice(attribute_sentences[attributes_list[1]])
+		third_pick = random.choice(attribute_sentences[attributes_list[2]])
+
+		bio = bio_format.format(new_names[i][0], first_pick, second_pick, third_pick)
+
+		if len(new_names[i]) == 2:
+			bio.strip()
+			nick_name = nickname_sentence.format(new_names[i][0], new_names[i][1])
+			bio += nick_name
+
+
+		bios.append(bio)
+
+		attribute_sentences[attributes_list[0]].remove(first_pick)
+		attribute_sentences[attributes_list[1]].remove(second_pick)
+		attribute_sentences[attributes_list[2]].remove(third_pick)
+
+	return bios
 
 if __name__=="__main__":
-	generate_SQL_insert('fill_names.txt', 'M', 'NA', ['O', 'A', 'B', 'S', 'T', 'L', 'C', 'F'])
-	print(create_bio('Axel', 'ABC'))
-
+	#generate_SQL_insert_random('fill_names.txt', 'M', 'NA', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+	generate_SQL_insert_chosen('fill_names.txt', 'M', 'NA')
+	for i in create_bio(['Billy Bill', 'Howard', 'Oscar'], 'AEG'):
+		print(i)
 
 
